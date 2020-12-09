ca4427e0898fdd4365d92e147b1c6d292a51b5b7
m-vojjala <mounikavojjala@gmail.com>
2020-12-09 17:42:25 -0500

    transformed data with selectors

---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+
+export function getAppointmentsForDay(state, day) {
+  let ids = [];
+  const appointments = [];
+  for (let item of state.days) {
+    if (item.name === day) {
+      ids = item.appointments;
+    }
+  }
+  for(let appointment in state.appointments){
+     if(ids.includes(Number(appointment))){
+    appointments.push(state.appointments[appointment]);
+      
+     }
+  }
+  return appointments;
+}
